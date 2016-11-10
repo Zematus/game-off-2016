ae02ab9356ba12fa1b8cdbdae5d7feed50a57fe0
Zematus <jb_tamez@hotmail.com>
2016-11-09 22:21:56 -0800

    Initial check
    - card images
    - base scene

---
+++ P0K3R/Assets/CardScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardScript : MonoBehaviour {
+
+	public Sprite[] Faces = new Sprite[52];
+
+	public Image Image;
+
+	// Use this for initialization
+	void Start () {
+
+		SetFace (12);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetFace (int index) {
+
+		Image.sprite = Faces [index];
+	}
+}
